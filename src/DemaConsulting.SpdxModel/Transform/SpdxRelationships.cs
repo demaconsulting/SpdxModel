ece2c0ae9d78118bfc771275ef4beed8d0b033ec
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-07-29 00:26:39 -0400

    Add relationships transform. (#41)

---
+++ src/DemaConsulting.SpdxModel/Transform/SpdxRelationships.cs
@@ -0,0 +1,63 @@
+﻿namespace DemaConsulting.SpdxModel.Transform;
+
+/// <summary>
+/// Transformations for SPDX relationships.
+/// </summary>
+public static class SpdxRelationships
+{
+    /// <summary>
+    /// Add new relationships to the SPDX document.
+    /// </summary>
+    /// <param name="document">SPDX document</param>
+    /// <param name="relationships">New relationships to add</param>
+    /// <param name="replace">True to replace existing relationships</param>
+    public static void Add(SpdxDocument document, IEnumerable<SpdxRelationship> relationships, bool replace = false)
+    {
+        // Handle replacing existing relationships
+        if (replace)
+        {
+            // Remove all relationships that refer to the same elements as the new relationships
+            document.Relationships = document.Relationships
+                .Where(r => !relationships.Any(
+                    r2 => SpdxRelationship.SameElements.Equals(r, r2)))
+                .ToArray();
+        }
+
+        // Add the new relationships
+        foreach (var relationship in relationships)
+        {
+            Add(document, relationship);
+        }
+    }
+
+    /// <summary>
+    /// Add a relationship to the SPDX document.
+    /// </summary>
+    /// <param name="document">SPDX document</param>
+    /// <param name="relationship">SPDX relationship to add</param>
+    /// <exception cref="ArgumentException">Thrown if the relationship argument is invalid</exception>
+    public static void Add(SpdxDocument document, SpdxRelationship relationship)
+    {
+        // Ensure the relationship ID matches an element
+        if (document.GetElement(relationship.Id) == null)
+            throw new ArgumentException($"Element {relationship.Id} not found in SPDX document", nameof(relationship));
+
+        // Ensure the relationship related-element ID matches an element
+        if (document.GetElement(relationship.RelatedSpdxElement) == null)
+            throw new ArgumentException($"Element {relationship.RelatedSpdxElement} not found in SPDX document", nameof(relationship));
+
+        // Look for an existing relationship
+        var existing = Array.Find(document.Relationships, r => SpdxRelationship.Same.Equals(r, relationship));
+        if (existing != null)
+        {
+            // Enhance the existing relationship
+            existing.Enhance(relationship);
+        }
+        else
+        {
+            // Copy the new relationship
+            existing = relationship.DeepCopy();
+            document.Relationships = document.Relationships.Append(existing).ToArray();
+        }
+    }
+}
---
+++ test/DemaConsulting.SpdxModel.Tests/Transforms/SpdxRelationshipsTests.cs
@@ -0,0 +1,234 @@
+﻿using DemaConsulting.SpdxModel.IO;
+using DemaConsulting.SpdxModel.Transform;
+
+namespace DemaConsulting.SpdxModel.Tests.Transforms;
+
+[TestClass]
+public class SpdxRelationshipsTests
+{
+    /// <summary>
+    /// Test SPDX document for relationships.
+    /// </summary>
+    private const string Contents = "{\r\n" +
+                                    "  \"files\": [],\r\n" +
+                                    "  \"packages\": [" +
+                                    "    {\r\n" +
+                                    "      \"SPDXID\": \"SPDXRef-Package-1\",\r\n" +
+                                    "      \"name\": \"Test Package\",\r\n" +
+                                    "      \"versionInfo\": \"1.0.0\",\r\n" +
+                                    "      \"packageFileName\": \"package1.zip\",\r\n" +
+                                    "      \"downloadLocation\": \"https://github.com/demaconsulting/SpdxTool\",\r\n" +
+                                    "      \"licenseConcluded\": \"MIT\"\r\n" +
+                                    "    },\r\n" +
+                                    "    {\r\n" +
+                                    "      \"SPDXID\": \"SPDXRef-Package-2\",\r\n" +
+                                    "      \"name\": \"Another Test Package\",\r\n" +
+                                    "      \"versionInfo\": \"2.0.0\",\r\n" +
+                                    "      \"packageFileName\": \"package2.tar\",\r\n" +
+                                    "      \"downloadLocation\": \"https://github.com/demaconsulting/SpdxModel\",\r\n" +
+                                    "      \"licenseConcluded\": \"MIT\"\r\n" +
+                                    "    }\r\n" +
+                                    "  ],\r\n" +
+                                    "  \"spdxVersion\": \"SPDX-2.2\",\r\n" +
+                                    "  \"dataLicense\": \"CC0-1.0\",\r\n" +
+                                    "  \"SPDXID\": \"SPDXRef-DOCUMENT\",\r\n" +
+                                    "  \"name\": \"Test Document\",\r\n" +
+                                    "  \"documentNamespace\": \"https://sbom.spdx.org\",\r\n" +
+                                    "  \"creationInfo\": {\r\n" +
+                                    "    \"created\": \"2021-10-01T00:00:00Z\",\r\n" +
+                                    "    \"creators\": [ \"Person: Malcolm Nixon\" ]\r\n" +
+                                    "  },\r\n" +
+                                    "  \"documentDescribes\": [ \"SPDXRef-Package-1\" ]\r\n" +
+                                    "}";
+
+    [TestMethod]
+    public void AddRelationshipBadId()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            SpdxRelationships.Add(
+                document, 
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-Missing",
+                    RelatedSpdxElement = "SPDXRef-Package-2",
+                    RelationshipType = SpdxRelationshipType.DependsOn
+                });
+        });
+
+        // Assert
+        Assert.AreEqual(0, document.Relationships.Length);
+    }
+
+    [TestMethod]
+    public void AddRelationshipBadRelatedElement()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            SpdxRelationships.Add(
+                document,
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-1",
+                    RelatedSpdxElement = "SPDXRef-Package-Missing",
+                    RelationshipType = SpdxRelationshipType.DependsOn
+                });
+        });
+
+        // Assert
+        Assert.AreEqual(0, document.Relationships.Length);
+    }
+
+    [TestMethod]
+    public void AddRelationship()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        SpdxRelationships.Add(
+            document,
+            new SpdxRelationship
+            {
+                Id = "SPDXRef-Package-1",
+                RelatedSpdxElement = "SPDXRef-Package-2",
+                RelationshipType = SpdxRelationshipType.DependsOn
+            });
+
+        // Assert
+        Assert.AreEqual(1, document.Relationships.Length);
+        Assert.AreEqual("SPDXRef-Package-1", document.Relationships[0].Id);
+        Assert.AreEqual("SPDXRef-Package-2", document.Relationships[0].RelatedSpdxElement);
+        Assert.AreEqual(SpdxRelationshipType.DependsOn, document.Relationships[0].RelationshipType);
+    }
+
+    [TestMethod]
+    public void AddRelationshipDuplicate()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        SpdxRelationships.Add(
+            document,
+            new SpdxRelationship
+            {
+                Id = "SPDXRef-Package-1",
+                RelatedSpdxElement = "SPDXRef-Package-2",
+                RelationshipType = SpdxRelationshipType.DependsOn
+            });
+        SpdxRelationships.Add(
+            document,
+            new SpdxRelationship
+            {
+                Id = "SPDXRef-Package-1",
+                RelatedSpdxElement = "SPDXRef-Package-2",
+                RelationshipType = SpdxRelationshipType.DependsOn
+            });
+
+        // Assert
+        Assert.AreEqual(1, document.Relationships.Length);
+        Assert.AreEqual("SPDXRef-Package-1", document.Relationships[0].Id);
+        Assert.AreEqual("SPDXRef-Package-2", document.Relationships[0].RelatedSpdxElement);
+        Assert.AreEqual(SpdxRelationshipType.DependsOn, document.Relationships[0].RelationshipType);
+    }
+
+    [TestMethod]
+    public void AddRelationships()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        SpdxRelationships.Add(
+            document,
+            new[]
+            {
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-1",
+                    RelatedSpdxElement = "SPDXRef-Package-2",
+                    RelationshipType = SpdxRelationshipType.DependsOn
+                }
+            });
+
+        // Assert
+        Assert.AreEqual(1, document.Relationships.Length);
+        Assert.AreEqual("SPDXRef-Package-1", document.Relationships[0].Id);
+        Assert.AreEqual("SPDXRef-Package-2", document.Relationships[0].RelatedSpdxElement);
+        Assert.AreEqual(SpdxRelationshipType.DependsOn, document.Relationships[0].RelationshipType);
+    }
+
+    [TestMethod]
+    public void AddRelationshipsDuplicate()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+
+        // Act
+        SpdxRelationships.Add(
+            document,
+            new[]
+            {
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-1",
+                    RelatedSpdxElement = "SPDXRef-Package-2",
+                    RelationshipType = SpdxRelationshipType.DependsOn
+                },
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-1",
+                    RelatedSpdxElement = "SPDXRef-Package-2",
+                    RelationshipType = SpdxRelationshipType.DependsOn
+                }
+            });
+
+        // Assert
+        Assert.AreEqual(1, document.Relationships.Length);
+        Assert.AreEqual("SPDXRef-Package-1", document.Relationships[0].Id);
+        Assert.AreEqual("SPDXRef-Package-2", document.Relationships[0].RelatedSpdxElement);
+        Assert.AreEqual(SpdxRelationshipType.DependsOn, document.Relationships[0].RelationshipType);
+    }
+
+    [TestMethod]
+    public void AddRelationshipsReplace()
+    {
+        // Arrange
+        var document = Spdx2JsonDeserializer.Deserialize(Contents);
+        SpdxRelationships.Add(
+            document,
+            new SpdxRelationship
+            {
+                Id = "SPDXRef-Package-1",
+                RelatedSpdxElement = "SPDXRef-Package-2",
+                RelationshipType = SpdxRelationshipType.DependsOn
+            });
+
+        // Act
+        SpdxRelationships.Add(document, new[]
+            {
+                new SpdxRelationship
+                {
+                    Id = "SPDXRef-Package-1",
+                    RelatedSpdxElement = "SPDXRef-Package-2",
+                    RelationshipType = SpdxRelationshipType.BuildToolOf
+                }
+            },
+            true);
+
+        // Assert
+        Assert.AreEqual(1, document.Relationships.Length);
+        Assert.AreEqual("SPDXRef-Package-1", document.Relationships[0].Id);
+        Assert.AreEqual("SPDXRef-Package-2", document.Relationships[0].RelatedSpdxElement);
+        Assert.AreEqual(SpdxRelationshipType.BuildToolOf, document.Relationships[0].RelationshipType);
+    }
+}
